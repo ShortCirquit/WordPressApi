e7d0a88bb0c93fae36bac92ef259945fb0d8888a
Nabeel Sulieman <nabsul@outlook.com>
2015-10-18 11:10:47 -0700

    Adding test model for comments

---
+++ Test/CommentTestModel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nabeel
+ * Date: 2015-10-17
+ * Time: 5:58 PM
+ */
+
+namespace ShortCirquit\WordPressApi\Test;
+
+
+class CommentTestModel
+{
+    public $id;
+    public $content;
+    public $likes;
+    public $isLiked;
+}
