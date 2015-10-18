a2a88fc6d1d7321c49c724afa18a2cac2ac20b16
Nabeel Sulieman <nabsul@outlook.com>
2015-10-18 11:06:27 -0700

    Add User and Post models for testing
    api-specific schemas will be converted to and from these models to allow common tests for both COM and ORG APIs.

---
+++ Test/PostTestModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nabeel
+ * Date: 2015-10-17
+ * Time: 5:58 PM
+ */
+
+namespace ShortCirquit\WordPressApi\Test;
+
+
+class PostTestModel
+{
+    public $id;
+    public $date;
+    public $title;
+    public $content;
+    public $likes;
+    public $hasLiked;
+    public $status;
+}
---
+++ Test/UserTestModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nabeel
+ * Date: 2015-10-17
+ * Time: 5:58 PM
+ */
+
+namespace ShortCirquit\WordPressApi\Test;
+
+
+class UserTestModel
+{
+    public $id;
+    public $name;
+}
